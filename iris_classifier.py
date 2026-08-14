599756cb262b550f4de64fb15ab7989d58dfa707
Novasol9! <rennensen.surejsen@gmail.com>
2026-08-14 15:21:52 +0530

    EDA for iris classifier

---
+++ iris_classifier.py
@@ -0,0 +1,28 @@
+ #""" 'created 13.082026'"""
+
+
+
+import numpy as np
+import pandas as pd
+
+from sklearn.datasets import load_iris
+iris=load_iris()
+desc=iris.DESCR
+
+df=pd.DataFrame(iris.data, columns=iris.feature_names)
+df['species']=iris.target
+
+corr=df.corr()
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.title('Feature Correlation Heatmap - Iris Dataset')
+
+sns.heatmap(corr, annot=True, cmap='coolwarm', fmt='.2f')
+
+df['species_name']=df['species'].map({0:'setosa',1:'versicolor',2:'virginia'})
+sns.pairplot(df,hue='species_name')
+
+plt.show()
+
