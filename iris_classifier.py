accaab24397e0d4f9f48b34c354dd98b1c4be045
Novasol9! <rennensen.surejsen@gmail.com>
2026-08-14 18:39:45 +0530

    Add train-test-split

--- iris_classifier.py
+++ iris_classifier.py
@@ -26,3 +26,21 @@ sns.pairplot(df,hue='species_name')
 
 plt.show()
 
+from sklearn.model_selection import train_test_split 
+
+
+
+
+X= df.drop(['species_name','species'] ,axis=1)
+y=df['species']
+
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=42,test_size=0.2 )
+
+
+
+
+
+
+
+
